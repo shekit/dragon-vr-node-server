47fbcf2d187e2a055fb65fdfb81f64dde0924fa9
shekit <as7790@nyu.edu>
2015-12-02 19:45:12 -0500

    initial commit with routes setup

---
+++ beep.js
@@ -0,0 +1,57 @@
+var io = require('socket.io')({
+	transports: ['websocket'],
+});
+
+// SOCKET CONNECTION
+
+io.attach(4567);
+
+
+io.on('connection', function(socket){
+	console.log('unity connected')
+	socket.on('beep', function(){
+		console.log("Got a beep")
+		io.emit('boop');
+	});
+
+	socket.on('disconnect', function(){
+		console.log("Unity disconnected")
+	})
+})
+
+var http = require('http');
+
+var Router = require('node-simple-router');
+var router = Router();
+
+var server = http.createServer(router);
+
+router.get("/left", function(request, response){
+	io.emit("left");
+	console.log("Left Left")
+	response.end("Left");
+})
+
+router.get("/right", function(request, response){
+	io.emit("right");
+	console.log("Right right")
+	response.end("Right");
+})
+
+router.get("/up", function(request, response){
+	io.emit("up");
+	console.log("UP UP and away")
+	response.end("Up")
+})
+
+router.get("/down", function(request, response){
+	io.emit("down");
+	console.log("DOWN DOWN")
+	response.end("Down")
+})
+
+server.listen(8080, function(){
+	console.log("Server listening on port")
+})
+
+
